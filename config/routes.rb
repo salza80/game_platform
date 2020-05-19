18bebe8fa3dc8d3f05bdee3aed00cb9c603450fb
Sally Mclean <smclean17@gmail.com>
2020-05-20 00:53:08 +0200

    remove develpment routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
     passwords: 'users/passwords'
   }
 
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
+  # if Rails.env.development?
+  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  # end
   post "/graphql", to: "graphql#execute"
 
   devise_for :admin_users, ActiveAdmin::Devise.config
