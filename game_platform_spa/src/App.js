bc2d09e01f37a344e79419c1ce872191ad87087f
Sally Mclean <smclean17@gmail.com>
2020-06-08 21:26:31 +0200

    add react-helmet fix react-snap staic render on deploy

--- game_platform_spa/src/App.js
+++ game_platform_spa/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 // import logo from './logo.svg';
 import Layout from './core/layout'
 import './App.css';
 
 function App() {
   return (
+    <React.Fragment>
+      <Helmet>
+        <title>German Games</title>
+          <meta name="description" content="Learn German with Games!" />
+          <meta property="og:url" content="https://deutsch.games" />
+          <meta property="og:title" content="Deutsch Games!" />
+          <meta property="og:description" content="Learn German with Games!" />
+          <meda property="og:image" content="https://deutsch.games/spa/logo512.png" />
+        </Helmet>
       <Layout />
+    </React.Fragment>
   );
 }
 
